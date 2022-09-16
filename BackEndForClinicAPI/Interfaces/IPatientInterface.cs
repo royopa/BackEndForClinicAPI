d19955b80b9100b1abe606c0ccbcabff9d66636d
Adam Ameen <adamameen5@gmail.com>
2022-09-16 17:50:39 +0530

    AA -  implemented interaces for Patient and Contact.

---
+++ BackEndForClinicAPI/Interfaces/IPatientInterface.cs
@@ -0,0 +1,15 @@
+﻿using BackEndForClinicAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndForClinicAPI.Interfaces
+{
+    public interface IPatientInterface
+    {
+        public Task<IActionResult> GetPatients();
+        public Task<IActionResult> GetPatient(Guid id);
+        public Task<IActionResult> AddPatient(AddPatientRequest addPatientRequest);
+        public Task<IActionResult> DeletePatient(Guid id);
+        public Task<IActionResult> UpdatePatient(Guid id, UpdatePatientRequest updatePatientRequest);
+
+    }
+}
