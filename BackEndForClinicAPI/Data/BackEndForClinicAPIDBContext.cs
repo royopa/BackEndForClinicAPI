e8585dacf449a2cb02123110294a31d7731c02ea
Adam Ameen <adamameen5@gmail.com>
2022-09-20 18:43:24 +0530

    Removed contacts model and classes which were used for testing.

--- BackEndForClinicAPI/Data/BackEndForClinicAPIDBContext.cs
+++ BackEndForClinicAPI/Data/BackEndForClinicAPIDBContext.cs
@@ -9,7 +9,6 @@ namespace BackEndForClinicAPI.Data
         {
         }
 
-        public DbSet<Contact> Contacts { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
 
