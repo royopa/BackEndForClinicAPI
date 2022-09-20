c9254a839bb281c4741ac5a95e5e3d4f28152456
Adam Ameen <adamameen5@gmail.com>
2022-09-20 19:24:19 +0530

    Changes to patient and doctor models with new migration scriprts.

--- BackEndForClinicAPI/Models/Patient.cs
+++ BackEndForClinicAPI/Models/Patient.cs
@@ -1,11 +1,7 @@
 ﻿namespace BackEndForClinicAPI.Models
 {
-    public class Patient
+    public class Patient : UserModel
     {
-        public Guid Id { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public long Phone { get; set; }
-        public string Address { get; set; }
+        public string InsuranceNumber { get; set; }
     }
 }
