cc0eaf5e94f360e18c17a5d72f3915bfb211ca11
Adam Ameen <adamameen5@gmail.com>
2022-09-20 14:55:22 +0530

    Implemented dummy authorization and authentication.

---
+++ BackEndForClinicAPI/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using BackEndForClinicAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BackEndForClinicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found.");
+        }
+
+        private object Generate(UserModel userModel)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userModel.UserName),
+                new Claim(ClaimTypes.Email, userModel.EmailAddress),
+                new Claim(ClaimTypes.GivenName, userModel.GivenName),
+                new Claim(ClaimTypes.Surname, userModel.Surname),
+                new Claim(ClaimTypes.Role, userModel.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials : credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault( o => o.UserName.ToLower() == userLogin.UserName.ToLower() && o.Password == userLogin.Password);
+            
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BackEndForClinicAPI/Models/UserConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEndForClinicAPI.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { UserName = "jason_admin", EmailAddress = "jason.admin@email.com", GivenName = "Json", Password = "MyPassW0rd", Surname = "Bryant", Role = "Administrator"},
+            new UserModel() { UserName = "elyse_seller", EmailAddress = "elyse.seller@email.com", GivenName = "Elyse", Password = "MyPassW0rd", Surname = "Mark", Role = "Seller"}
+        };
+    }
+}
